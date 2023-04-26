a6349e4f2ecfec2dd93ccf8e393a1451843df493
Shmouel kessous <chmouel2690@gmail.com>
2023-04-26 16:11:18 +0300

    fix dark vail and footer

--- src/components/footer/MainFooter.tsx
+++ src/components/footer/MainFooter.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const MainFooter = () => {
     return (
-        <div className=' h-[25vh] '>
+        <div className='  '>
             <div className="SocialIcons flex rtl:flex-row-reverse justify-center  space-x-3  my-5">
                 <i className={`bg-[url('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png')] w-[40px] h-[40px] bg-contain`}></i>
               <div className='bg-white w-[40px] h-[40px] rounded-xl'>
@@ -11,7 +11,7 @@ export const MainFooter = () => {
                 <i className={`bg-[url('https://cdn-icons-png.flaticon.com/512/124/124034.png?w=360')] w-[40px] h-[40px] bg-contain rounded-xl`}></i>
             
             </div>
-            <p className='text-center text-xs  text-black font-semibold  pb-[40px]'>
+            <p className='text-center text-xs  text-black font-semibold  '>
                 Powered by Chmouel Kessous, Designed by Rebecca Salmanowitz
                 @All rights reserved to Rivka Nakache Cosmetics
             </p>
--- src/components/special/DarkVail.tsx
+++ src/components/special/DarkVail.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const DarkVail = ({children}:{children:React.ReactNode}) => {
     return (
-        <div className='  absolute   right-0 z-50 w-full min-h-screen  bg-black bg-opacity-50'>
+        <div className='  absolute   right-0 top-0 bottom-0 z-50 w-full min-h-full  bg-black bg-opacity-50'>
             {children}
         </div>
     )
