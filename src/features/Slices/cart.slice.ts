25c65c03e27cb115599b2ef05cc987d793315759
Shmouel kessous <chmouel2690@gmail.com>
2023-06-18 18:38:06 +0300

    trying solution

--- src/features/Slices/cart.slice.ts
+++ src/features/Slices/cart.slice.ts
@@ -1,88 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { Cart, productFromDB } from "../../interfaces";
+import { Cart, ProductInCart, SpecificationDto, productFromDB } from "../../interfaces";
 import { PayloadAction } from "@reduxjs/toolkit/src";
-
 
 
 const initialState: Cart = {
     products: []
 }
+interface addToCartAction {
+    baseProduct: productFromDB,
+    specId: SpecificationDto
+}
 
-
-const addToCartAction = (data: productFromDB[], product: productFromDB) => {
-    if (data.length === 0) {
-        console.log('length == 0');
-
-        data.push({ ...product, count: 1 });
-        return data;
-    }
-    for (let i = 0; i < data.length; i++) {
-
-        if (data[i].id === product.id) {
-            data[i].count++;
-
-            break;
-        }
-        else if (i == data.length - 1) {
-
-            data.push({ ...product, count: 1 });
-            break;
-        }
-
-    }
-
-
-    return data;
-};
-
-export const cartSlice = createSlice({
-    name: 'cartSlice',
+const CartSlice = createSlice({
     initialState,
+    name: "cart",
     reducers: {
-        addToCart: (state, action: PayloadAction<productFromDB>) => {
-
-            state.products = addToCartAction(state.products, action.payload);
-            state.totalInCart++;
-            state.totalPrice += action.payload.selling_price
-            updateLocalStorage(state.products, state.totalInCart);
-
-        },
-        subItem: (state, action: PayloadAction<string>) => {
-            state.products.map((p, i) => {
-                if (p.id === action.payload && p.count) {
-                    p.count--;
-                    state.totalInCart--;
-                }
-                if (p.id === action.payload && p.count === 0) {
-                    state.products.splice(i, 1);
-                    console.log('triggerd', state.totalInCart);
-
-                }
-            })
-            console.log('ouside of map', state.totalInCart);
-            updateLocalStorage(state.products, state.totalInCart);
-        },
-        removeFromCart: (state, action: PayloadAction<string>) => {
-            state.products = state.products.filter(product => {
-                if (product.id !== action.payload)
-                    return product
-                else if (product.count) state.totalInCart -= product.count;
-            })
-            updateLocalStorage(state.products, state.totalInCart)
-        },
-        clearCart: (state) => {
-            state = initialState
-            localStorage.removeItem(CART_NAME)
-        },
-        loadCart: (state, action: PayloadAction<{ product: productFromDB[], count: number }>) => {
-            state.products = action.payload.product
-            state.totalInCart = action.payload.count
-            // console.log('loaded',{products},{payload});
+        addProduct(state, { payload: { name, imgUrl, id } }: PayloadAction<productFromDB>) {
+            const toPush: ProductInCart = {
+                name,
+                imgUrl,
+                product_id: id,
+                spec: [{}]
+            }
+            state.products.push()
         }
-
-    },
-
+    }
 })
-
-export const { subItem, addToCart, clearCart, removeFromCart, loadCart } = cartSlice.actions
-export default cartSlice.reducer
--- src/interfaces/cart.interface.ts
+++ src/interfaces/cart.interface.ts
@@ -8,6 +8,19 @@ export interface ProductInCart {
     imgUrl: string
     name: string
     description: string
-    spec: SpecificationDto
-    count: number
+    spec: specInCart
 }
+export interface specInCart {
+    spec: SpecificationDto[],
+    count: 0
+}
+
+/*
+* step 1:   product id exist in products ? 
+    ? true :
+        * spec exist in  product[id].spec ?
+            ?true : product[id].spec[color].counter ++ 
+            !false : product[id].spec.push(spec);
+    !false : 
+        !products.push(product{...prod,spec:[{...spec}]})
+ */
